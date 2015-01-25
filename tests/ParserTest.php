494dcc41e850b495a3a44aac3d2847e7df733dd8
Alexander Noriega <lambdatoast@gmail.com>
2015-01-25 19:37:53 -0300

    Add success test for product of str.

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -2,9 +2,17 @@
 
 class ParserTest extends \PHPUnit_Framework_TestCase
 {
-    public function testStr() {
+    public function testStrSuccesses() {
       $this->assertTrue(
         Parser::str('abc')->run('abcdef')->equal(new Good('abc', 3))
+      );
+
+      $this->assertTrue(
+        Parser::product(
+          Parser::str('ab'),
+          Parser::str('cd')
+        )->run('abcd')
+         ->equal(new Good(array('ab', 'cd'), 2))
       );
     }
 }
