327123391c24a426fea37c09db5caa5c91dbb1f1
Alexander Noriega <lambdatoast@gmail.com>
2015-01-25 20:20:34 -0300

    Implement Equal for Location.

--- src/Location.php
+++ src/Location.php
@@ -1,24 +1,32 @@
 <?php
 
-class Location {
+class Location implements Equal {
   private $input;
   private $offset = 0;
+
   function __construct($i, $o = 0) {
     $this->input = $i;
     $this->offset = $o;
   }
+
   function toError($msg) {
     return new ParserError(array(array($this, $msg)));
   }
+
   function advanceBy($n) {
     return new Location($this->input, $this->offset + $n);
   }
+
   function input() {
     return $this->input;
   }
+
   function offset() {
     return $this->offset;
   }
+
+  function equal($l) {
+    return $this->input  === $l->input && 
+           $this->offset === $l->offset;
+  }
 }
-
-
