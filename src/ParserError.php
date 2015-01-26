f528fd9452ebc035d55d7e48502796f7b34c2611
Alexander Noriega <lambdatoast@gmail.com>
2015-01-25 22:19:06 -0300

    Add latest() function to ParserError.

--- src/ParserError.php
+++ src/ParserError.php
@@ -2,9 +2,16 @@
 
 class ParserError {
   var $errors = array();
+
   function __construct($errors) {
     $this->errors = $errors;
   }
+
+  function latest() {
+    $copy = array_merge($this->errors, array());
+    return array_pop($copy);
+  }
+
 }
 
 
