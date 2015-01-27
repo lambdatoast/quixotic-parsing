a49b1ee2985c785b99c42b139cdcf5f64fba94fb
Alexander Noriega <lambdatoast@gmail.com>
2015-01-26 21:13:45 -0300

    Add docs and times() function to Parsing interface.

--- src/Parsing.php
+++ src/Parsing.php
@@ -2,15 +2,51 @@
 
 interface Parsing extends Alternative, Chain, Functor {
 
-  // A -> Parser[A]
+  /**
+   * Parser that always succeeds with value $a.
+   * @param mixed $a.
+   * @return Parsing
+   */
   static function succeed($a);
-  // Char -> Parser[Char]
+
+  /**
+   * @param string $a Should be a single character.
+   * @return Parsing
+   */
   static function char($c);
-  // String -> Parser[String]
+
+  /**
+   * @param string $s
+   * @return Parsing
+   */
   static function str($s);
-  static function map2(Parser $a, Parser $b, callable $f);
-  static function product(Parser $a, Parser $b);
-  // String -> Result[A]
+
+  /**
+   * @param Parsing $p1
+   * @param Parsing $p2
+   * $param callable $f Takes two results and returns something.
+   * @return Parsing
+   */
+  static function map2(Parser $p1, Parser $p2, callable $f);
+
+  /**
+   * @param Parsing $p1
+   * @param Parsing $p2
+   * @return Parsing Parsing whose running Result is tuple of the two parsers.
+   */
+  static function product(Parser $p1, Parser $p2);
+
+  /**
+   * @param int $n
+   * @param Parsing $p
+   * @return Parsing Parsing whose running Result is array of $n values.
+   */
+  static function times($n, Parser $p);
+
+  /**
+   * @param String $input
+   * @return Result
+   */
   function run($input);
 
 }
