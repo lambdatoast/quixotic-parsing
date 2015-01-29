f316782e8922ea7fd9f476b75022079678aae098
Alexander Noriega <lambdatoast@gmail.com>
2015-01-29 00:23:52 -0300

    Improve test messages and use the right order of arguments for assertEquals().

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -25,54 +25,55 @@ class ParserTest extends \PHPUnit_Framework_TestCase {
       );
 
       $this->assertEquals(
+        new Good(array('ab', 'cd'), 4),
         Parser::product(
           Parser::str('ab'),
           Parser::str('cd')
         )->run('abcd'),
-        new Good(array('ab', 'cd'), 4),
-        'Parsing product'
+        'Parser::product() of two successful parsers is an array of each respective result.'
       );
 
       $this->assertEquals(
+        new Good('ABC', 6),
         Parser::str('abc')->chain(function ($s) {
           return Parser::str(strtoupper($s));
-        })->run('abcABC'), new Good('ABC', 6),
-        'Contextual parsing'
+        })->run('abcABC'),
+        'Parser::chain() allows context-sensitive parsing and the character consumption is correctly managed.'
       );
 
       $this->assertEquals(
-        Parser::times(3, Parser::str('ab'))->run('ababab'),
         new Good(array('ab', 'ab', 'ab'), 6),
+        Parser::times(3, Parser::str('ab'))->run('ababab'),
         'Parser::times(n, p) when successful must produce list of n results.'
       );
 
       $count = function ($xs) { return count($xs); };
 
       $this->assertEquals(
-        Parser::times(3, Parser::str('ab'))->map($count)->run('ababab'),
         new Good(3, 6),
+        Parser::times(3, Parser::str('ab'))->map($count)->run('ababab'),
         'Parser::times(n, p)->map(count) when successful must produce n.'
       );
     }
 
     public function testRegex() {
       $this->assertEquals(
-        Parser::regex('/^a(\w)c$/')->run('abc'),
         new Good(array('abc', 'b'), 3),
+        Parser::regex('/^a(\w)c$/')->run('abc'),
         'Parser::regex(pattern) when successful must produce an array of matches.'
       );
     }
 
     public function testTextParsing() {
       $this->assertEquals(
-        Parser::satisfyStr(function ($s) { return $s === 'ab'; })->run('ab'),
         new Good('ab', 2),
+        Parser::satisfyStr(function ($s) { return $s === 'ab'; })->run('ab'),
         'Parser::satisfyStr(f) when successful must produce the input.'
       );
 
       $this->assertEquals(
-        Parser::anyChar()->run('a'),
         new Good('a', 1),
+        Parser::anyChar()->run('a'),
         'Parser::anyChar() when successfully run with a char c must produce c.'
       );
 
@@ -83,8 +84,8 @@ class ParserTest extends \PHPUnit_Framework_TestCase {
       );
 
       $this->assertEquals(
-        Parser::anyChar()->run(''),
         new Bad((new Location('', 0))->toError("Did not satisfy predicate with input ''")),
+        Parser::anyChar()->run(''),
         'Parser::anyChar() fails when run on an empty string.'
       );
 
